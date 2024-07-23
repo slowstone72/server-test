6a3d67c41d0420c79e0056f8a4587a14ff3561de
slowstone72 <callumfisher@gmail.com>
2024-07-23 21:56:50 +0100

    same as before, oops

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 /* server test
 edited: 2024.7.23 */
 
-// test
+// import { Server } from "socket.io";
+import express from "express";
+import { createServer } from "node:http";
+
+console.log("beep boop, looks like I'm on the air");
+
+const port = process.env.PORT ?? 1234;
+
+const app = express();
+const server = createServer(app);
+
+server.listen(port, () => {
+    console.log(`server launched @ ${port}`);
+});
+
+app.get("/", (req, res) => {
+    res.send("<u>hello, looks like i'm internetting</u>");
+});
